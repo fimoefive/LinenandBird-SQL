c146ae0e7811c0d4851c6cb383e96a02a7b26cd6
Martin Sisk <Brothermj@hotmail.com>
2021-09-17 22:16:49 -0500

    added commented notes for output inserted. * command.

--- CRUD.sql
+++ CRUD.sql
@@ -9,6 +9,9 @@ INSERT INTO [dbo].[Orders]
            ([BirdId]
            ,[HatId]
            ,[Price])
+
+output inserted. * -- output command that only shows what is being inserted into the table
+
      VALUES
            ('f9ffafee-a95d-443d-b547-fefa7ffa2c89'
            ,'5d1a5e75-1265-41d5-809f-3451ccd27edc'
