da54fb2291595872c8eb56de88918956afbd32d1
Martin Sisk <Brothermj@hotmail.com>
2021-09-17 21:59:12 -0500

    inserted and orders query in the CRUD.sql file.

---
+++ CRUD.sql
@@ -0,0 +1,15 @@
+-- insert rows into a table
+
+USE [LinenandBird]
+GO
+
+INSERT INTO [dbo].[Orders]
+           ([BirdId]
+           ,[HatId]
+           ,[Price])
+     VALUES
+           ('f9ffafee-a95d-443d-b547-fefa7ffa2c89'
+           ,'5d1a5e75-1265-41d5-809f-3451ccd27edc'
+           ,45)
+GO
+
